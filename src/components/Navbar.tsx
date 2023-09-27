af13e5caa05a40b36bfe4c4f5731c3a8d86b72e0
Mushfikur Rahman <mushfikur0@gmail.com>
2023-09-27 20:14:01 +0100

    Solve issue with button not triggering

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,26 @@
 import { UserCircle2 } from "lucide-react";
-import { Button } from "./ui/button";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="fixed top-0 inset-x-0 h-fit0 z-[10] py-3 border-border border-b">
       <div className="container flex justify-between items-center">
         <div className="flex items-center h-full">
-          <p className="font-medium text-lg">BookingApp.</p>
+          <Link href="/">
+            <p className="font-medium text-lg">BookingApp.</p>
+          </Link>
         </div>
 
-        <Button
-          variant="ghost"
-          className="flex justify-center items-center gap-2"
+        <Link
+          className={buttonVariants({ variant: "secondary" })}
+          href="/login"
         >
-          <UserCircle2 className="h-4 w-4 text-foreground" />
-          <p className="text-sm font-light text-foreground">Login</p>
-        </Button>
+          <div className="flex justify-center items-center gap-2">
+            <UserCircle2 className="h-4 w-4 text-foreground" />
+            <p className="text-sm text-foreground">Login</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
