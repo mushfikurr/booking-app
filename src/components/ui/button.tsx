3bd7daae40e6b548e49a8678df2d0053f454d549
Mushfikur Rahman <mushfikur0@gmail.com>
2023-09-28 21:36:16 +0100

    Overwrite shad-cn button for loading variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -36,11 +37,43 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
   asChild?: boolean;
 }
 
+const ButtonLoading = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, children, asChild = false, ...props }, ref) => {
+    return (
+      <Button
+        disabled
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+      >
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        {children}
+      </Button>
+    );
+  }
+);
+
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, variant, size, isLoading = false, asChild = false, ...props },
+    ref
+  ) => {
+    // Extend shadcn component to allow for loading buttons by passing isLoading prop in. (works with custom theme)
+    if (isLoading) {
+      return (
+        <ButtonLoading
+          className={className}
+          variant={variant}
+          size={size}
+          isLoading={isLoading}
+          asChild={asChild}
+        />
+      );
+    }
+
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
