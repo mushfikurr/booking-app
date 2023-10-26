67e5bd0b1f4ce7cb5162c56360dbee124ec39211
mushfikurr <mushfikur0@gmail.com>
2023-10-26 14:47:33 +0100

    Better if statement for layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,22 +13,22 @@ export default async function Layout({
   const data = await getServerSession(authOptions);
   const user = data?.user;
 
-  if (user?.isBusinessUser) {
-    return (
-      <div className="container max-w-5xl flex flex-col gap-5">
-        <h1 className="text-4xl font-bold">Dashboard</h1>
-        <RouteTabs className="w-full flex flex-col gap-4 mb-16">
-          <TabsList className="grid grid-cols-4 w-fit">
-            <TabsTrigger value="overview">Overview</TabsTrigger>
-            <TabsTrigger value="services">Services</TabsTrigger>
-            <TabsTrigger value="bookings">Bookings</TabsTrigger>
-            <TabsTrigger value="editProfile">Edit Profile</TabsTrigger>
-          </TabsList>
-          <Suspense fallback={<h1>Loading!!</h1>}>{children}</Suspense>
-        </RouteTabs>
-      </div>
-    );
-  } else {
+  if (!user?.isBusinessUser) {
     redirect("/profile");
   }
+
+  return (
+    <div className="container max-w-5xl flex flex-col gap-5">
+      <h1 className="text-4xl font-bold">Dashboard</h1>
+      <RouteTabs className="w-full flex flex-col gap-4 mb-16">
+        <TabsList className="grid grid-cols-4 w-fit">
+          <TabsTrigger value="overview">Overview</TabsTrigger>
+          <TabsTrigger value="services">Services</TabsTrigger>
+          <TabsTrigger value="bookings">Bookings</TabsTrigger>
+          <TabsTrigger value="editProfile">Edit Profile</TabsTrigger>
+        </TabsList>
+        <Suspense fallback={<h1>Loading!!</h1>}>{children}</Suspense>
+      </RouteTabs>
+    </div>
+  );
 }
