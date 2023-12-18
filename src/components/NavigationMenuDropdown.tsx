fde425b8255b118c8e5cbe1ed3b5f9c3e85e9a82
Mushfikur Rahman <mushfikur0@gmail.com>
2023-12-18 20:22:48 +0000

    Make navigation menu dropdown dynamic (structure routes as objects)

--- src/components/NavigationMenuDropdown.tsx
+++ src/components/NavigationMenuDropdown.tsx
@@ -1,50 +1,111 @@
 import { cn } from "@/lib/utils";
-import { LogOut, User2 } from "lucide-react";
+import { LogOut, LucideIcon, User2 } from "lucide-react";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { NavigationMenuLink } from "./ui/navigation-menu";
 import { Separator } from "./ui/separator";
 
+const routes = [
+  {
+    Icon: User2,
+    title: "Dashboard",
+    href: "/dashboard/overview",
+  },
+  {
+    Icon: LogOut,
+    title: "Logout",
+    handleClick: () => {
+      signOut();
+      redirect("/");
+    },
+  },
+];
+
+const businessRoutes = [
+  { Icon: User2, title: "Profile", href: "/profile" },
+  ...routes.slice(1),
+];
+
+const renderNavigationItem = (route: any) => {
+  if ("href" in route)
+    return (
+      <NavigationMenuDropdownLink
+        {...(route as NavigationMenuDropdownLinkProps)}
+      />
+    );
+  else
+    return (
+      <NavigationMenuDropdownButton
+        {...(route as NavigationMenuDropdownButtonProps)}
+      />
+    );
+};
+
 export default function NavigationMenuDropdown({
   isBusinessUser,
 }: {
   isBusinessUser: boolean;
 }) {
+  const routesToDisplay = isBusinessUser ? businessRoutes : routes;
+  const displayedRoutes = routesToDisplay.map((route) =>
+    renderNavigationItem(route)
+  );
+
+  return <ul className="flex flex-col p-3 sm:w-[260px]">{displayedRoutes}</ul>;
+}
+
+interface NavigationMenuDropdownItem {
+  Icon: LucideIcon;
+  title: string;
+}
+
+interface NavigationMenuDropdownLinkProps extends NavigationMenuDropdownItem {
+  href: string;
+}
+function NavigationMenuDropdownLink({
+  Icon,
+  title,
+  href,
+}: NavigationMenuDropdownLinkProps) {
   return (
-    <ul className="flex flex-col p-3 sm:w-[200px]">
-      <NavigationMenuLink asChild>
-        <Link
-          className={cn(
-            "block select-none space-y-1 rounded-sm p-2 leading-none text-foreground/80 no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-          )}
-          href={isBusinessUser ? "/dashboard/overview" : "/profile"}
-        >
-          <div className="flex items-center gap-3 text-sm font-medium leading-none">
-            <User2 className="w-4 h-4" />
-            <p className="leading-snug">
-              {isBusinessUser ? "Dashboard" : "Profile"}
-            </p>
-          </div>
-        </Link>
-      </NavigationMenuLink>
-      <Separator className="my-2" />
-      <NavigationMenuLink asChild>
-        <button
-          className={cn(
-            "block select-none space-y-1 rounded-sm p-2 leading-none text-foreground/80 no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-          )}
-          onClick={() => {
-            signOut();
-            redirect("/");
-          }}
-        >
-          <div className="flex items-center gap-3 text-sm font-medium leading-none">
-            <LogOut className="w-4 h-4" />
-            <p className="leading-snug">Logout</p>
-          </div>
-        </button>
-      </NavigationMenuLink>
-    </ul>
+    <NavigationMenuLink asChild>
+      <Link
+        className={cn(
+          "block select-none space-y-1 rounded-sm p-2 leading-none text-foreground/80 no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+        )}
+        href={href}
+      >
+        <div className="flex items-center gap-3 text-sm font-medium leading-none">
+          <Icon className="w-4 h-4" />
+          <p className="leading-snug">{title}</p>
+        </div>
+      </Link>
+    </NavigationMenuLink>
+  );
+}
+
+interface NavigationMenuDropdownButtonProps extends NavigationMenuDropdownItem {
+  handleClick: () => void;
+}
+function NavigationMenuDropdownButton({
+  Icon,
+  title,
+  handleClick,
+}: NavigationMenuDropdownButtonProps) {
+  return (
+    <NavigationMenuLink asChild>
+      <button
+        className={cn(
+          "block select-none space-y-1 rounded-sm p-2 leading-none text-foreground/80 no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+        )}
+        onClick={handleClick}
+      >
+        <div className="flex items-center gap-3 text-sm font-medium leading-none">
+          <LogOut className="w-4 h-4" />
+          <p className="leading-snug">Logout</p>
+        </div>
+      </button>
+    </NavigationMenuLink>
   );
 }
